afc48bd6b131f65022e47311c7042711187ba6ea
Alan Lu <apialan0334@cchntek.com>
2018-12-13 14:20:08 +0800

    First commit.

---
+++ snowflake.go
@@ -0,0 +1,32 @@
+package snowflake
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"sync/atomic"
+	"time"
+)
+
+func init() {
+	go resetSequenceLoop()
+}
+
+// seq 序號
+var seq uint32
+
+// resetSequenceLoop 重置序號
+func resetSequenceLoop() {
+	for range time.Tick(time.Second) {
+		atomic.StoreUint32(&seq, 0)
+	}
+}
+
+// Get 取得
+func Get() uint64 {
+	fmt.Printf("ts\t%d\npid\t%d\nseq\t%d\n", time.Now().Unix(), os.Getpid(), seq)
+	s := fmt.Sprintf("%d%d%d", time.Now().Unix(), os.Getpid(), atomic.LoadUint32(&seq))
+	atomic.StoreUint32(&seq, atomic.AddUint32(&seq, 1))
+	n, _ := strconv.ParseUint(s, 10, 64)
+	return n
+}
---
+++ snowflake_test.go
@@ -0,0 +1,9 @@
+package snowflake
+
+import (
+	"testing"
+)
+
+func TestGet(t *testing.T) {
+	println(Get())
+}
